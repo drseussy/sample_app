0ce80513df1f8cfff0b3f07db8b2850baae142cb
Stirling Larson <stirlingdavidlarson@gmail.com>
2012-06-14 10:08:27 -0400

    Added a stub User show page

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,6 @@ ActiveRecord::Schema.define(:version => 20120613205810) do
     t.datetime "updated_at", :null => false
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email"
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end

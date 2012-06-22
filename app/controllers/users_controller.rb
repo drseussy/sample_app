2e230f3555f2143ecb297376f245af0081543ee9
Stirling Larson <stirlingdavidlarson@gmail.com>
2012-06-22 11:37:10 -0400

    Can now destroy users (as admins)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:edit, :edit, :update]
+  before_filter :authenticate, :only => [:edit, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
   
   def index
     @users = User.paginate(:page => params[:page])
@@ -29,12 +30,10 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
     @title = "Edit user"
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       redirect_to @user, :flash => { :success => "Profile updated." }
     else
@@ -42,6 +41,11 @@ class UsersController < ApplicationController
       render 'edit'
     end 
   end 
+  
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to user_path, :flash => { :success => "User destroyed." }
+  end
 
   private
   
@@ -53,5 +57,10 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
+    
+  def admin_user
+    user = User.find(params[:id])
+    redirect_to(root_path) unless (current_user.admin? && !current_user?(@user))
+  end 
 end
       
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -149,6 +149,27 @@ describe User do
     
     it "should return the user on an email/password match" do
       User.authenticate(@attr[:email], @attr[:password]).should == @user
+    end
+  end
+  
+  describe "admin attribute" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should respond to admin" do
+      @user.should respond_to(:admin)
+    end
+    
+    it "should not be an admin by default" do
+      @user.should_not be_admin
+    end
+    
+    it "should be convertible to admin"
+      @user.toggle!(:admin)
+      @user.should be_admin
+  
   end
 end
 

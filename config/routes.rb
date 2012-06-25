1a8fa16df767f0f74b4832e34ad4eed933362121
Stirling Larson <stirlingdavidlarson@gmail.com>
2012-06-25 14:30:02 -0400

    microposts interface

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ SampleApp::Application.routes.draw do
   
 
   resources :users
-  resources :sessions, :only => [:new, :create, :destroy]
+  resources :sessions,   :only => [:new, :create, :destroy]
+  resources :microposts, :only => [:create, :destroy]
   
   root :to => 'pages#home'
   

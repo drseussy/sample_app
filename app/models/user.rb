3743a407cbcc20134ed6d08009cb99f909486e9d
Stirling Larson <stirlingdavidlarson@gmail.com>
2012-06-28 09:53:12 -0400

    User relationships (following/followers)

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,15 @@ class User < ActiveRecord::Base
   attr_accessor   :password
   attr_accessible :email, :name, :password, :password_confirmation
   
-  has_many :microposts, :dependent => :destroy
+  has_many :microposts,    :dependent => :destroy
+  has_many :relationships, :dependent => :destroy,
+                           :foreign_key => "follower_id"
+  has_many :reverse_relationships, :dependent => :destroy,
+                                   :foreign_key => "followed_id",
+                                   :class_name => "Relationship"
+  has_many :following, :through => :relationships, :source => :followed
+  has_many :followers, :through => :reverse_relationships,
+                       :source => :follower
   
   email_regex = /\A[\w+\-.]+@[a-z\d.]+\.[a-z]+\z/i
   
@@ -40,6 +48,17 @@ class User < ActiveRecord::Base
     Micropost.where("user_id = ?", id)
   end
   
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+  
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+  
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
+  end
   class << self 
     def authenticate(email, submitted_password)
       user = find_by_email(email)
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -43,3 +43,14 @@ describe Micropost do
 end
 
 
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
